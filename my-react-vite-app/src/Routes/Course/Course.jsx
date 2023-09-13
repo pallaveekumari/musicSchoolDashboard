14d679be029d3fefcc41e845944a379f433dfef7
pallaveekumari <pallavee.kumari@razorpay.com>
2023-09-13 22:06:45 +0530

    added search functionality

--- my-react-vite-app/src/Routes/Course/Course.jsx
+++ my-react-vite-app/src/Routes/Course/Course.jsx
@@ -1,143 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Course.module.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import { courseList } from "../../assets/courseList";
 
 const Course = () => {
-  const courseList = [
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-    {
-      Name: "Classical Guitar",
-      Description: "Classical Guitar For...",
-      Instructor: "Ms. jane Doe",
-      Instrument: "Guitar",
-      Day_of_Week: "Wednesday",
-      _of_Students: "26",
-      Price: "$60",
-      Status: "Active",
-      Actions: ":",
-    },
-  ];
+  const [courseData, setCourseData] = useState(courseList);
 
+  const handleSearch = (val) => {
+    if(val=="" || val == undefined || val == null){
+      setCourseData(courseList);
+    }
+    else{
+
+      let searchedResult = courseData.filter((el, i) => {
+        return (
+          el.Name.includes(val) ||
+          el.Description.includes(val) ||
+        el.Instructor.includes(val) ||
+        el.Instrument.includes(val) ||
+        el.Day_of_Week.includes(val) ||
+        el._of_Students.includes(val) ||
+        el.Price.includes(val) ||
+        el.Status.includes(val) ||
+        el.Actions.includes(val)
+        );
+      });
+    setCourseData(searchedResult)
+  }
+  };
   return (
     <div className={styles.mainContainer}>
       <Sidebar />
@@ -147,43 +37,45 @@ const Course = () => {
           <div className={styles.subHeading}>Courses List</div>
           <div>
             <input
+            onChange={(e)=>handleSearch(e.target.value)}
               className={styles.input}
               type="text"
               placeholder="Search here"
             />
           </div>
         </div>
-
-        <table className={styles.table} >
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Instructor</th>
-              <th>Instrument</th>
-              <th>Day_of_Week</th>
-              <th>#_of_Students</th>
-              <th>Price</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {courseList.map((el, index) => (
-              <tr key={index}>
-                <td>{el.Name}</td>
-                <td>{el.Description}</td>
-                <td>{el.Instructor}</td>
-                <td>{el.Instrument}</td>
-                <td>{el.Day_of_Week}</td>
-                <td>{el._of_Students}</td>
-                <td>{el.Price}</td>
-                <td>{el.Status}</td>
-                <td>{el.Actions}</td>
+        <div className={styles.tableContainer}>
+          <table className={styles.table}>
+            <thead>
+              <tr className={styles.row}>
+                <th className={styles.td}>Name</th>
+                <th className={styles.td}>Description</th>
+                <th className={styles.td}>Instructor</th>
+                <th className={styles.td}>Instrument</th>
+                <th className={styles.td}>Day_of_Week</th>
+                <th className={styles.td}>#_of_Students</th>
+                <th className={styles.td}>Price</th>
+                <th className={styles.td}>Status</th>
+                <th className={styles.td}>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {courseData.map((el, index) => (
+                <tr key={index} className={styles.rows}>
+                  <td className={styles.td}>{el.Name}</td>
+                  <td className={styles.td}>{el.Description}</td>
+                  <td className={styles.td}>{el.Instructor}</td>
+                  <td className={styles.td}>{el.Instrument}</td>
+                  <td className={styles.td}>{el.Day_of_Week}</td>
+                  <td className={styles.td}>{el._of_Students}</td>
+                  <td className={styles.td}>{el.Price}</td>
+                  <td className={styles.td}>{el.Status}</td>
+                  <td className={styles.td}>{el.Actions}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
