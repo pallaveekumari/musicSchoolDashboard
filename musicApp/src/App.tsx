ec4ca39882b4d4f99c2f3485deefadf6bbbd0da2
pallaveekumari <pallavee.kumari@razorpay.com>
2023-09-13 11:54:03 +0530

    initial-commit

---
+++ musicApp/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Overview from './Routes/Overview'
+import { Box } from 'lucide-react'
+
+function App() {
+
+
+  return (
+    <>
+      <Box>
+        <Overview/>
+       </Box>
+    </>
+  )
+}
+
+export default App
---
+++ musicApp/src/Routes/overview.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from '@/components/ui/button'
+
+
+
+const Overview = () => {
+  return (
+    <Button >Click me</Button>
+  )
+}
+
+export default Overview
