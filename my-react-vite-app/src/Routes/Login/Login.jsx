1beb2a1c77beb04fd1cab0b3adeeb3b660d076d1
pallaveekumari <pallavee.kumari@razorpay.com>
2023-09-13 22:31:16 +0530

    coursepage-responsive-completed

--- my-react-vite-app/src/Routes/Login/Login.jsx
+++ my-react-vite-app/src/Routes/Login/Login.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
-import styles from "../Login/Login.module.css"
-import axios from "axios"
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
 
-const navigate=useNavigate()
+  const handleLogin =async () => {
+    const [formData,setFormData] = useState({email:'',password:''})
+  let res = await  axios.post("https://reqres.in/api/login",formData);
+    let data = res.data;
+    console.log(data);
+    if(res.data.token){
+        localStorage.setItem('token',JSON.stringify(res.data.token))
+    }
+  };
 
-const handleLogin=()=>{
-    axios.post('https://reqres.in/api/login', {
-        "email":"eve.holt@reqres.in",
-    "password":"cityslicka"
-      });
-}
+  const handleChange=(e)=>{
+const {name,value} = e.target;
+setFormData({...formData,[name]:value})
+  }
 
   return (
     <div className={styles.mainContainer}>
-    <div className={styles.formContainer}>
+      <div className={styles.formContainer}>
         <h1>Login</h1>
         <br />
-        
-        <div className={styles.eachTextFieldBox}>
-            <input  placeholder="Email" type="email"/>
 
+        <div className={styles.eachTextFieldBox}>
+          <input name="email" onChange={(e)=>handleChange(e)} placeholder="Email" type="email" />
         </div>
         <div className={styles.eachTextFieldBox}>
-            <input  placeholder="Password" type="password" />
-
+          <input name='password' onChange={(e)=>handleChange(e)} placeholder="Password" type="password" />
         </div>
-        <button className={styles.signUpBtn} onClick={()=>{
-            handleLogin()
-            alert("Login Successfully")
+        <button
+          className={styles.signUpBtn}
+          onClick={async () => {
+           await handleLogin();
+            alert("Login Successfully");
             // localStorage.setItem("token")
-            navigate("/course")
-        }}>Login</button>
+            navigate("/course");
+          }}
+        >
+          Login
+        </button>
+      </div>
     </div>
-    
+  );
+};
 
-</div>
-  )
-}
-
-export default Login
+export default Login;
