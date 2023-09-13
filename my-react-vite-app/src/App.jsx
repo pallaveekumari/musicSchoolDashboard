dab6eb61265d06198a806285d4c03bdc82b5f516
pallaveekumari <pallavee.kumari@razorpay.com>
2023-09-13 14:24:24 +0530

    initial-commit

---
+++ my-react-vite-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+      <div className="bg-red-500 text-white p-4">This is a Tailwind CSS component.</div>
+
+        </div>
+    </>
+  )
+}
+
+export default App
